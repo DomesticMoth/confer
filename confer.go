19a1abdeaf37b736387cb4cf523e51eaa53d4167
DomesticMoth <91414737+DomesticMoth@users.noreply.github.com>
2022-01-14 19:40:59 +0000

    Add comments

--- confer.go
+++ confer.go
@@ -8,17 +8,20 @@ import (
 )
 
 
-
+// Load configuration from toml file located on the path passed in the arguments or on one of the default paths.
 func LoadConfig(paths []string, conf interface{}) (err error){
+	// If the path is passed through arguments
 	if len(os.Args) > 1 {
+		// Put it at the top of the list of paths
 		paths = append(os.Args[1:2], paths...)
 	}
+	// An error for the case if there is no path
 	err = errors.New("No path was passed to find the configuration file")
+	// Trying each of the paths
 	for _, path := range paths {
 		_, err = toml.DecodeFile(path, conf)
-		if err == nil {
-			return
-		}
+		// If config was loaded, return the result
+		if err == nil { return }	
 	}
 	return
 }
